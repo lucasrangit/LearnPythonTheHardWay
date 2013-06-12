717b24c3d843b4ca59901284b65f36b2684ca839
Lucas Magasweran <external+github.com@magasweran.com>
2013-06-11 18:12:06 -0700

    completed exercise on raw_input()

---
+++ ex11.py
@@ -0,0 +1,12 @@
+print "How old are you?", 
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall, and %r heavy." % (age, height, weight)
+
+s = raw_input('--> ')
+print s
+
