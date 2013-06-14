089549d8245d0532c8eaf1242c81aaa6e40cf70f
Lucas Magasweran <external+github.com@magasweran.com>
2013-06-13 17:47:23 -0700

    add raw_input() to argv example

--- ex13.py
+++ ex13.py
@@ -2,10 +2,12 @@
 
 from sys import argv
 
+x = raw_input('How many input parameters? ')
+
 script, first, second, third = argv
 
 print "The script is called:", script
 print "Your first variables is:", first
 print "Your second variables is:", second
 print "Your third variable is:", third
-
+print "There are 4 input parameters, you guessed %r" % int(x)
