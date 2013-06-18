3c1ca9f33b620e13a22fe41d3eb6c501f4572b93
Lucas Rangit MAGASWERAN <external+github.com@magasweran.com>
2013-06-17 21:23:19 -0700

    symbols in python used thus far

---
+++ ex22.py
@@ -0,0 +1,17 @@
+# Exercise 22: What do you know so far?
+
+# Commands I have used this far:
+float(string)
+raw_input(file)
+open(filename)
+from os.path import exists
+from sys import argv
+f.seek(0)
+exists(filename):
+f.read()
+f.close()
+print()
+len(data)
+return 
+f.truncate()
+f.write()
