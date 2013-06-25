ff3f0af8a5180c563ac5dd51d4601b79e06e6e54
Lucas Rangit MAGASWERAN <external+github.com@magasweran.com>
2013-06-24 21:35:59 -0700

    fixed the syntax errors

--- ex26.py
+++ ex26.py
@@ -7,15 +7,15 @@ def sort_words(words):
     """Sorts the words."""
     return sorted(words)
 
-def print_first_word(words)
+def print_first_word(words):
     """Prints the first word after popping it off."""
-    word = words.poop(0)
-    print word
+    word = words.pop(0)
+    print words
 
 def print_last_word(words):
     """Prints the last word after popping it off."""
-    word = words.pop(-1
-    print word
+    word = words.pop(-1)
+    print words
 
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
@@ -57,13 +57,13 @@ print "This should be five: %s" % five
 
 def secret_formula(started):
     jelly_beans = started * 500
-    jars = jelly_beans \ 1000
+    jars = jelly_beans / 1000
     crates = jars / 100
     return jelly_beans, jars, crates
 
 
 start_point = 10000
-beans, jars, crates == secret_formula(start-point)
+beans, jars, crates = secret_formula(start_point)
 
 print "With a starting point of: %d" % start_point
 print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
@@ -71,22 +71,21 @@ print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
 start_point = start_point / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_pont
+print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_point)
 
+sentence = "All good\tthings come to those who wait."
 
-sentence = "All god\tthings come to those who weight."
-
-words = ex25.break_words(sentence)
-sorted_words = ex25.sort_words(words)
+words = break_words(sentence)
+sorted_words = sort_words(words)
 
 print_first_word(words)
 print_last_word(words)
-.print_first_word(sorted_words)
+print_first_word(sorted_words)
 print_last_word(sorted_words)
-sorted_words = ex25.sort_sentence(sentence)
-prin sorted_words
+sorted_words = sort_sentence(sentence)
+print sorted_words
 
-print_irst_and_last(sentence)
+print_first_and_last(sentence)
 
-   print_first_a_last_sorted(senence)
+print_first_and_last_sorted(sentence)
 
