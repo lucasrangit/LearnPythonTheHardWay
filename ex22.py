076d87a4318c9fc5709051f9855b85d301cb9120
Lucas Rangit MAGASWERAN <external+github.com@magasweran.com>
2013-06-24 20:22:30 -0700

    documented functions i've learned

--- ex22.py
+++ ex22.py
@@ -1,17 +1,45 @@
 # Exercise 22: What do you know so far?
-
 # Commands I have used this far:
-float(string)
-raw_input(file)
-open(filename)
+
+# Convert a string to a floating point number if possible
+class float(object)
+
+# Read a string from stdin
+raw_input([prompt])
+
+# open a file
+open(name[, mode[, buffering]]))
+
+# inport OS POSIX path function exists
 from os.path import exists
+
+# test whether a path exists
+exists(path)
+
+# import from objects maintained by interpreter the command line arg support 
 from sys import argv
-f.seek(0)
-exists(filename):
-f.read()
+
+# move file to new position
+f.seek(offset[, whence])
+
+# read at most size bytes, returned as a string
+f.read([size])
+
+# close the file
 f.close()
+
+# truncate the file to at most size bytes
+f.truncate([size])
+
+# write string to file
+f.write(str)
+
+# write string to stdout
 print()
-len(data)
+
+# return the number of items of a sequence or mapping
+len(object)
+
+# return control to calling function and return expression as value
 return 
-f.truncate()
-f.write()
+
