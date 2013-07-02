9e1ea7a33b7a00be305851191fa9b7972b402e94
Lucas Rangit MAGASWERAN <external+github.com@magasweran.com>
2013-07-01 21:01:03 -0700

    conditionals based on user input

---
+++ ex31.py
@@ -0,0 +1,36 @@
+# Exercise 31: Making Decisions
+
+print "You enter a dark room with two doors. Do you go through door #1 or door #2?"
+
+door = raw_input("> ")
+
+if door == "1":
+	print "There'sa giant bear here eating cheese cake. What do you do?"
+	print "1. Take the cake."
+	print "2. Scream at the bear."
+
+	bear = raw_input('> ')
+
+	if bear == "1":
+		print "The bear eats your face off. Good job!"
+	elif bear == "2":
+		print "The bear eats your legs off. Good job!"
+	else:
+		print "Well, doing %s is probably better. Bear runs away." % bear
+
+elif door == "2":
+	print "You stare in to the endless abyss at Cthulhu's retina."
+	print "1. Blueberries."
+	print "2. Yellow jacket clothspins."
+	print "3. Understanding revolvers yelling melodies."
+
+	insanity = raw_input('> ')
+
+	if insanity == "1" or insanity == "2":
+		print "Your body survives powered by a mind of jell-o. Good job!"
+	else:
+		print "The insanity rots your eyes into a pool of muck. Good job!"
+
+else:
+	print "You stumble around and fall on a knife and die. Good job!"
+
