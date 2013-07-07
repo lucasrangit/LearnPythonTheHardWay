babed7920a2571568e4b27a69f3116bd0f0d4036
Lucas Rangit MAGASWERAN <external+github.com@magasweran.com>
2013-07-07 15:59:05 -0700

    list indexing using cardinal (0 index) and ordinal (1 index)

---
+++ ex34.py
@@ -0,0 +1,10 @@
+# Exercise 43: Accessing Elements Of Lists
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The first (ordinal) animal is at 0 and is a", animals[0]
+print "The animal at 0 (cardinal) is a", animals[0]
+print "The third (ordinal) animal is at 2 and is a", animals[2]
+print "The animal at 2 (cardinal) is a", animals[2]
+
+print "The year 2010 in \"January 1, 2010\" is an ordinal number since years start at 1"
