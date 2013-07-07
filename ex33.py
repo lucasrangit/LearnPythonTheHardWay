ad251922e37a08ae974a1fb8c190ae95f375d57e
Lucas Rangit MAGASWERAN <external+github.com@magasweran.com>
2013-07-07 15:17:55 -0700

    refactor and use foor loop, take in argument for range and increment

--- ex33.py
+++ ex33.py
@@ -1,16 +1,21 @@
 # Exercise 33: While Loops
 
-i = 0
 numbers = []
 
-while i < 6:
-	print "At the top i is %d" % i
-	numbers.append(i)
+def list_append_num(num, inc):
+	i = 0
+	#while i < num:
+	for i in range(0,num):
+		print "At the top i is %d" % i
+		numbers.append(i)
 
-	i = i + 1
-	print "Numbers now: ", numbers
-	print "At the bottom i is %d" % i
+		#i = i + inc
+		print "Numbers now: ", numbers
+		print "At the bottom i is %d" % i
 
+num_max = int(raw_input('Max: '))
+num_inc = int(raw_input('Increment: '))
+list_append_num(num_max, num_inc)
 print "The numbers: "
 
 for num in numbers:
