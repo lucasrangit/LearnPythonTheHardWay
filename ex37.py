a9906a2e1b793ed9f46dfa5e21b629ee3b6fb646
Lucas Rangit MAGASWERAN <external+github.com@magasweran.com>
2013-07-08 22:00:18 -0700

    review #2 start

---
+++ ex37.py
@@ -0,0 +1,215 @@
+# Keywords Review #2
+
+# and
+# a boolean operator that evaluates to True is both LHS and RHS are True
+
+# del
+# 
+
+# from
+# import _from_ a module
+
+# not
+# boolean operator to negate
+
+# while
+# a loop that evaluates the statements within if the statement is True
+
+# as 
+# ? defines a different logical name of an imported module
+
+# elif
+# multiple if-statement
+
+# global
+# ? a directive to the parser
+
+# or
+# boolean operator that evaluates to true if either LHS or RHS is True
+
+# with
+# ? 
+
+# assert
+# ? a convenient way to insert debug assertions
+
+# else
+# the catch all statement for an if-statement
+
+# if
+# a conditional test that executes the statements within if True
+
+# pass
+# ?
+
+# yield
+# ?
+
+# break
+# ?
+
+# except
+# ?
+
+# import
+# find a module and import the symbols
+
+# print
+# print a string to stdout
+
+# class
+# a definition of a logical grouping of variables and functions
+
+# exec
+# ?
+
+# in 
+# ?
+
+# raise
+# ?
+
+# continue
+# ?
+
+# finally
+# ?
+
+# is
+# ?
+
+# return
+# return execution to the calling function and optionally return an object from the callee
+
+# def
+# define a function
+
+# for
+# loop a specified number of times
+
+# lambda
+# ?
+
+# try
+# ?
+
+## Data Types
+
+# True
+# False
+# boolean
+
+# None
+# ? the null object
+
+# strings
+# ASCII 
+
+# numbers
+# 1, 2, 3, 4, ...
+
+# floats
+# 1.0, 1.1, ... 
+# lists
+
+## String Escape Sequences
+
+# %d
+# %i
+# %u
+# signed integer decimal
+
+# %o
+# signed octal value
+
+# %x
+# signed hexidecimal (lowercase)
+
+# %X
+# signed hexidecimal (uppercase)
+
+# %e
+# floating point exponential (lowercase)
+
+# %E
+# floating point exponential (uppercase) 
+
+# %f
+
+# %F
+
+# %g
+
+# %G
+
+# %c
+
+# %r
+
+# %s
+
+# %%
+
+## Operators
+
+# +
+
+# -
+
+# *
+
+# **
+
+# /
+
+# //
+
+# %
+
+# <
+
+# >
+
+# <=
+
+# >=
+
+# ==
+
+# !=
+
+# <>
+
+# ()
+
+# []
+
+# {}
+
+# @
+
+# ,
+
+# :
+
+# .
+
+# .
+
+# ;
+
+# +=
+
+# -=
+
+# *=
+
+# /=
+
+# //=
+
+# %=
+
+# **=
+
+
