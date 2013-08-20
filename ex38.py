656c5c2057ef5b74f16391eab91937c0e39097d4
Lucas Rangit MAGASWERAN <external+github.com@magasweran.com>
2013-08-19 21:12:25 -0700

    translated object calls to generic module calls

--- ex38.py
+++ ex38.py
@@ -2,17 +2,23 @@
 
 ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
-print "Wait there's not 10 things in that list,let's fix that."
+print "Wait there's not 10 things in that list, let's fix that."
 
 stuff = ten_things.split(' ')
-stuff = split( ten_things, ' ')
+print stuff
+stuff = str.split(ten_things, ' ')
+print stuff
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
 while len(stuff) != 10:
 	next_one = more_stuff.pop()
 	print "Adding: ", next_one
 	stuff.append(next_one)
-	append(stuff, next_one)
+	print "There's %d items now." % len(stuff)
+
+	next_one = list.pop(more_stuff) 	
+	print "Adding: ", next_one
+	list.append(stuff, next_one)
 	print "There's %d items now." % len(stuff)
 
 print "There we go: ", stuff
@@ -23,10 +29,10 @@ print stuff[1]
 print stuff[-1] # whoa! fancy
 
 print stuff.pop()
-print pop(stuff)
+print list.pop(stuff)
 
 print ' '.join(stuff) # what? cool!
-print join(' ', stuff)
+print str.join(' ', stuff)
 
 print '#'.join(stuff[3:5]) # supper stellar!
-print join('#', stuff[3:5])
+print str.join('#', stuff[3:5])
